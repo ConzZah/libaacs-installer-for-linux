01fa85658985e279838286246fdee1195b228b43
ConzZah <69615452+ConzZah@users.noreply.github.com>
2024-06-27 06:07:23 +0200

    v2 RELEASED

--- libaacs-installer_latest.sh
+++ libaacs-installer_latest.sh
@@ -1,21 +1,38 @@
 #!/usr/bin/env bash
   #========================================
-  # Project: LIBAACS INSTALLER v1.2.1
+  # Project: LIBAACS INSTALLER v2
   # Author:  ConzZah / ©️ 2024
-  # Last Modification: 03.06.2024 / 07:07
+  # Last Modification: 26.06.2024 / 06:04
   #========================================
-clear; echo "[ ConzZah's LIBAACS INSTALLER v1.2 ]"; echo ""
-echo "THIS SCRIPT WILL DOWNLOAD & INSTALL THE FOLLOWING PACKAGES:"; echo ""
-echo "- vlc" 
-echo "- libaacs0" 
-echo "- libbluray-bdj" 
-echo "- libbluray2"
-echo "- keydb.cfg"
-echo ""; echo "[ PRESS ANY KEY TO START ]"; read -n 1 -s; echo ""
-sudo apt install -y vlc libaacs0 libbluray-bdj libbluray2 >/dev/null 2>&1
-echo ""; echo "[ DONE INSTALLING LIBAACS ]"; echo ""; echo ""
-echo "[ DOWNLOADING KEYDB.CFG FROM: ]"; echo "[ https://archive.org/download/keydb_eng/keydb_eng.zip ]"; echo ""; echo "[ PLEASE WAIT ]"; mkdir -p ~/.config/aacs/; cd ~/.config/aacs/
-wget -q -O last_updated.txt https://gist.githubusercontent.com/ConzZah/5631e144da455e9c08daa1dc80a01cf2/raw/
-cat last_updated.txt; echo ""; rm last_updated.txt; wget -q --show-progress -O keydb_eng.zip https://archive.org/download/keydb_eng/keydb_eng.zip; echo ""; echo "[ EXTRACTING KEYDB.CFG.. ]"
-7z x keydb_eng.zip >/dev/null 2>&1; mv keydb.cfg KEYDB.cfg; rm keydb_eng.zip; echo ""; echo "[ DONE INSTALLING LIBACCS & KEYDB.cfg , ENJOY YOUR BLU-RAYS! ]"; echo ""
-echo "[ PRESS ANY KEY TO EXIT ]"; read -n 1 -s; exit
+function main {
+_install="deb_install"
+done_msg="[ DONE INSTALLING LIBAACS ]"
+is_alpine=$(uname -v|grep -o -w Alpine)
+if [[ "$is_alpine" == "Alpine" ]]; then _install="alp_install"; fi
+clear; echo "[ ConzZah's LIBAACS INSTALLER v2 ]"; echo ""
+echo "THIS WILL DOWNLOAD & INSTALL THE FOLLOWING PACKAGES:"; echo ""
+echo "- vlc"
+echo "- libaacs"
+echo "- libbluray"
+echo "- 7z & base64 (for converting & extracting keydb.cfg)"
+echo ""; echo "[ PRESS ANY KEY TO START ]"; echo ""; read -n 1 -s;
+$_install
+}
+# install for debian:
+function deb_install {
+sudo apt install -y vlc libaacs0 libbluray-bdj libbluray2 7zip base64 >/dev/null 2>&1
+echo ""; echo "$done_msg"; echo ""; keydb
+}
+# install for alpine:
+function alp_install {
+doas apk add vlc-qt libaacs libbluray 7zip base64 >/dev/null 2>&1
+echo ""; echo "$done_msg"; echo ""; keydb
+}
+# download & installation of keydb.cfg
+function keydb {
+echo "[ DOWNLOADING KEYDB.CFG ]"; mkdir -p ~/.config/aacs/; cd ~/.config/aacs/
+wget -q -O keydb_eng.zip.txt https://gist.github.com/ConzZah/f6a71135f5b273a918419dc3c43cd4fd/raw; base64 -d keydb_eng.zip.txt > keydb_eng.zip;
+echo ""; echo "[ EXTRACTING KEYDB.CFG.. ]"; echo ""; 7z x keydb_eng.zip >/dev/null 2>&1; mv keydb.cfg KEYDB.cfg; rm keydb_eng.zip.txt && rm keydb_eng.zip
+echo "[ DONE INSTALLING LIBACCS & KEYDB.cfg , ENJOY YOUR BLU-RAYS! ]"; echo ""; echo "[ PRESS ANY KEY TO EXIT ]"; echo ""; read -n 1 -s; exit
+}
+main
